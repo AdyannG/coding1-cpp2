feecfac6b08e84a9b1a89b8736f494809d677515
AdyannG <gilbertadyann@gmail.com>
2025-02-27 12:07:45 -0600

    Fixed issues and nearly completed 5favgames

--- Coding2/5FavGames.cpp
+++ Coding2/5FavGames.cpp
@@ -2,8 +2,6 @@
 #include <string>
 using namespace std;
 								
-// This code is broken to all hell and I have no idea what happend, It never checked any of my if statements and just played them in order regardless of input and while I was working on it curly brackets got reassinged. 
-// I have given up for now because I've done nothing wrong from what I can see and have no idea what has happend. 
 int main() {
 	cout << "What are your top 5 games? \n"; 
 	cout << "You can...\n 'add' a game.\n 'edit' a game on the list.\n 'remove' a game from the list.\n 'show' your current list.\n 'quit' to leave the program.\n You can only have 5 games\n";
@@ -15,13 +13,13 @@ int main() {
 		cout << "Would you like to add, edit, remove, show, or quit? \n";
 		getline(cin, input);
 		 
-		if(input == "add"); {
+		if(input == "add") {
 			cout << "Please enter game title.\n";
 			getline(cin, input);
 			favGames[currentGames++] = input;
 		}
 			
-		if(input == "edit"); {
+		else if(input == "edit") {
 			cout << "What game would you like to edit?\n";
 			getline(cin, input);
 			for (int i = 0; i < currentGames; i++) {
@@ -32,25 +30,25 @@ int main() {
 					cout << "Game has been updated";
 				}
 			}
-
 		}
 			
-		if (input == "remove"); {
+		else if (input == "remove") {
 			cout << "What game would you like to remove?";
 			getline(cin, input);
 			for (int i = 0; i < currentGames; i++) {
 				if (favGames[i] == input) {
 					cout << "I have found the game you're looking for, deleting it now.\n";
-					favGames[i] = input;
+					favGames[i] = "";
 					cout << "Game has been removed";
 				}
-
 			}
-			if (input == "show"); {
-				cout << "Here are your Games.";
-				for (int i = 0; i < currentGames; i++);
-
-				if (input == "quit"); {
+		}
+		else if (input == "show") {
+			cout << "Here are your Games.";
+			for (int i = 0; i < currentGames; i++) {
+			}
+		}
+				else if (input == "quit") {
 					cout << "See ya later!";
 					break;
 				}
